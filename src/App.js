94132de05c4a48ba1922562803706421d5c6fc0f
Zeus-2321 <109408148+Zeus-2321@users.noreply.github.com>
2023-01-10 21:33:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HigherOrderFunctions from './components/HigherOrderComponents';
+
+function App() {
+  return (
+    <div className="App">
+      <HigherOrderFunctions />
+    </div>
+  );
+}
+
+export default App;
